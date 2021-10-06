a1a591ee81fb594668bb0c8c53734db6bb6fdea0
ronbarrantes <ronbarrantes@gmail.com>
2021-10-05 23:21:12 -0700

    Modify package.json and remove old components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Board from './Board'
 
-const App = () => (<Board />)
+const App = () => (
+    <div>Hello world</div>
+)
 export default App
--- src/components/Todo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-interface ITodo{
-    item: string;
-}
-
-const Todo = (props: ITodo) => (<div>{props.item}</div>)
-export default Todo
