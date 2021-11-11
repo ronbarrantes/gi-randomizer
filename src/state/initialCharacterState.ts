1a19554c45745a25229c699e76446b9424d00614
ronbarrantes <ronbarrantes@gmail.com>
2021-11-10 22:18:20 -0800

    Add an initial char state

---
+++ src/state/initialCharacterState.ts
@@ -0,0 +1,44 @@
+export const characterState = [
+	{
+		id: 1,
+		name: 'Amber',
+		isSelectable: true,
+		isAvailable: true,
+	},
+	{
+		id: 2,
+		name: 'Eula',
+		isSelectable: true,
+		isAvailable: true,
+	},
+	{
+		id: 3,
+		name: 'Ganyu',
+		isSelectable: true,
+		isAvailable: false,
+	},
+	{
+		id: 4,
+		name: 'Diluc',
+		isSelectable: true,
+		isAvailable: true,
+	},
+	{
+		id: 5,
+		name: 'Hu Tao',
+		isSelectable: true,
+		isAvailable: true,
+	},
+	{
+		id: 6,
+		name: 'Diona',
+		isSelectable: true,
+		isAvailable: true,
+	},
+	{
+		id: 7,
+		name: 'Barbara',
+		isSelectable: true,
+		isAvailable: true,
+	},
+]
