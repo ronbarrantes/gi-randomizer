597ea2e11f0ecf85dbb63b4b41b8aea8ba111a6d
ronbarrantes <ronbarrantes@gmail.com>
2021-11-11 18:20:41 -0800

    Done for the night :)

--- src/components/RandomizedTeam.tsx
+++ src/components/RandomizedTeam.tsx
@@ -1,22 +1,18 @@
 import React, { useReducer } from 'react'
 import { selectedCharacterReducer } from '../reducers/selectedCharacterReducer'
-import { charactersAvailable } from '../reducers/characterReducer'
+import { charactersAvailable, CharacterInfo } from '../reducers/characterReducer'
 import { randomizeCharacters } from '../utils'
 import { useCharacters } from '../components/CharacterContext'
 import CharacterCard from './CharacterCard'
 
-// type HandleCharacterProp = (id: number) => void
-
 export const RandomizedTeam: React.FC = () => {
 	const [state, dispatch] = useReducer(selectedCharacterReducer, [])
-	const chars = useCharacters()
+	let chars: CharacterInfo[] = useCharacters()
 
 	const handleRandomize = () => {
 	 	const randomizedTeam = randomizeCharacters(charactersAvailable(chars))
 		dispatch({ type: 'RANDOMIZE_CHARACTERS', characters: randomizedTeam })
 	}
-
-	// const handleAvailableChars = useCharUpdate()
 
 	return (
 		<div>
@@ -25,7 +21,6 @@ export const RandomizedTeam: React.FC = () => {
 				{state.map(char => <li key={char.id}>
 					<CharacterCard
 						id={char.id}
-						// handleAvailable={handleAvailableChars as HandleCharacterProp}
 						name={char.name}
 						isAvailable={char.isAvailable}
 						isSelectable={char.isSelectable}
