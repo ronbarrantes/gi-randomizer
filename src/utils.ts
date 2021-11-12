f85d3b609709a831f71acb2195206f4538a8eca3
ronbarrantes <ronbarrantes@gmail.com>
2021-11-11 18:19:32 -0800

    BUGFIX: Util fix

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,10 @@ import { State } from './reducers/characterReducer'
 
 export const randomizeCharacters = (state: State) => {
 	let chars: State = []
-	while(chars.length < 4){
+
+	const numOfChars = state.length >= 4 ? 4 : state.length
+
+	while(chars.length < numOfChars){
 		const randNumber = Math.floor(Math.random() * state.length)
 		const char = state.splice(randNumber, 1)[0]
 
