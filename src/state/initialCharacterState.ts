52918e74bd8e47185eb400264374d73a7be6e8ab
ronbarrantes <ronbarrantes@gmail.com>
2021-11-12 22:23:18 -0800

    Change spacing for initial state

--- src/state/initialCharacterState.ts
+++ src/state/initialCharacterState.ts
@@ -1,55 +1,54 @@
 import { CharacterInfo } from '../reducers/characterReducer'
 
 export const charactersList: string[] = [
-	'Traveler',
+    'Traveler',
 	// Mondstadt
-	'Amber',
-	'Kaeya',
-	'Lisa',
-	'Diluc',
-	'Mona',
-	'Venti',
-	'Diona',
-	'Noelle',
-	'Jean',
-	'Klee',
-	'Rosaria',
-	'Bennet',
-	'Albedo',
-	'Fishl',
-	'Eula',
+    'Amber',
+    'Kaeya',
+    'Lisa',
+    'Diluc',
+    'Mona',
+    'Venti',
+    'Diona',
+    'Noelle',
+    'Jean',
+    'Klee',
+    'Rosaria',
+    'Bennet',
+    'Albedo',
+    'Fishl',
+    'Eula',
 
 	// Liyue
-	'Xiangling',
-	'Ningguan',
-	'Xingqiu',
-	'Zhongli',
-	'Keqing',
-	'Chongyun',
-	'Xinyan',
-	'Ganyu',
-	'LAWYER',
-	'Beidou',
+    'Xiangling',
+    'Ningguan',
+    'Xingqiu',
+    'Zhongli',
+    'Keqing',
+    'Chongyun',
+    'Xinyan',
+    'Ganyu',
+    'LAWYER',
+    'Beidou',
 
 	// Inazuma
-	'Kasuha',
-	'Sayu',
-	'Raiden',
-	'Kokomi',
-	'Yoimiya',
-	'Ayaka',
-	'Sarah',
-
-	'Gorou',
-	'Itto',
+    'Kasuha',
+    'Sayu',
+    'Raiden',
+    'Kokomi',
+    'Yoimiya',
+    'Ayaka',
+    'Sarah',
+    'Gorou',
+    'Itto',
 ]
 
 const buildState = (characters: string[]): CharacterInfo[] =>
-	characters.map((char: string, i: number) => ({
-		name: char,
-		id: i,
-		isSelectable: true,
-		isAvailable: true,
-	}))
+    characters.map((char: string, i: number) => ({
+        name: char,
+        id: i,
+        isSelectable: true,
+        isAvailable: true,
+    }))
 
 export const characterState: CharacterInfo[] = buildState(charactersList)
