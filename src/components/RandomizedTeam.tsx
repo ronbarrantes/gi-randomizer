c0561d9cb33aaba4dac4836fc157820c62b61b66
ronbarrantes <ronbarrantes@gmail.com>
2021-11-12 22:21:32 -0800

    Change spacing

--- src/components/RandomizedTeam.tsx
+++ src/components/RandomizedTeam.tsx
@@ -6,29 +6,29 @@ import { useCharacters } from '../components/CharacterContext'
 import CharacterCard from './CharacterCard'
 
 export const RandomizedTeam: React.FC = () => {
-	const [state, dispatch] = useReducer(selectedCharacterReducer, [])
-	let chars: CharacterInfo[] = useCharacters()
+    const [state, dispatch] = useReducer(selectedCharacterReducer, [])
+    let chars: CharacterInfo[] = useCharacters()
 
-	const handleRandomize = () => {
-	 	const randomizedTeam = randomizeCharacters(charactersAvailable(chars))
-		dispatch({ type: 'RANDOMIZE_CHARACTERS', characters: randomizedTeam })
-	}
+    const handleRandomize = () => {
+        const randomizedTeam = randomizeCharacters(charactersAvailable(chars))
+        dispatch({ type: 'RANDOMIZE_CHARACTERS', characters: randomizedTeam })
+    }
 
-	return (
-		<div>
-			<button onClick={handleRandomize}>Randomize</button>
-			<ul>
-				{state.map(char => <li key={char.id}>
-					<CharacterCard
-						id={char.id}
-						name={char.name}
-						isAvailable={char.isAvailable}
-						isSelectable={char.isSelectable}
-					/>
-				</li>)}
-			</ul>
-		</div>
-	)
+    return (
+        <div>
+            <button onClick={handleRandomize}>Randomize</button>
+            <ul>
+                {state.map(char => <li key={char.id}>
+                    <CharacterCard
+                        id={char.id}
+                        name={char.name}
+                        isAvailable={char.isAvailable}
+                        isSelectable={char.isSelectable}
+                    />
+                </li>)}
+            </ul>
+        </div>
+    )
 }
 
 export default RandomizedTeam
